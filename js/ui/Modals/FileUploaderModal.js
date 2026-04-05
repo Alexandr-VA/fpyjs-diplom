ff02f028a555cefcb6d931793796680c02cd70b4
Alexandr-VA <alexandrlobash@gmail.com>
2026-04-05 21:45:56 +0300

    Update FileUploaderModal.js

--- js/ui/Modals/FileUploaderModal.js
+++ js/ui/Modals/FileUploaderModal.js
@@ -2,49 +2,76 @@
  * Класс FileUploaderModal
  * Используется как всплывающее окно для загрузки изображений
  */
-class FileUploaderModal {
-  constructor( element ) {
-
+class FileUploaderModal extends BaseModal {
+  constructor(element) {
+    super(element);
+    this.content = this.element.find('.content');
+    this.registerEvents();
   }
 
-  /**
-   * Добавляет следующие обработчики событий:
-   * 1. Клик по крестику на всплывающем окне, закрывает его
-   * 2. Клик по кнопке "Закрыть" на всплывающем окне, закрывает его
-   * 3. Клик по кнопке "Отправить все файлы" на всплывающем окне, вызывает метод sendAllImages
-   * 4. Клик по кнопке загрузке по контроллерам изображения: 
-   * убирает ошибку, если клик был по полю вода
-   * отправляет одно изображение, если клик был по кнопке отправки
-   */
-  registerEvents(){
-
+  registerEvents() {
+    this.element.find('.header .x.icon, .close.button').on('click', () => this.close());
+    this.element.find('.send-all.button').on('click', () => this.sendAllImages());
+    
+    this.content.on('click', (e) => {
+      const target = $(e.target);
+      if (target.hasClass('input') || target.closest('.input').length) {
+        target.closest('.input').removeClass('error');
+      }
+      if (target.is('button') || target.closest('button')) {
+        const container = target.closest('.image-preview-container');
+        if (container) {
+          this.sendImage(container);
+        }
+      }
+    });
   }
 
-  /**
-   * Отображает все полученные изображения в теле всплывающего окна
-   */
   showImages(images) {
-
+    const reversed = [...images].reverse();
+    const html = reversed.map(url => this.getImageHTML(url)).join('');
+    this.content.html(html);
   }
 
-  /**
-   * Формирует HTML разметку с изображением, полем ввода для имени файла и кнопкной загрузки
-   */
   getImageHTML(item) {
-
+    return `
+      <div class="image-preview-container">
+        <img src="${item}" />
+        <div class="ui action input">
+          <input type="text" placeholder="Путь к файлу">
+          <button class="ui button"><i class="upload icon"></i></button>
+        </div>
+      </div>
+    `;
   }
 
-  /**
-   * Отправляет все изображения в облако
-   */
   sendAllImages() {
-
+    const containers = this.content.find('.image-preview-container');
+    containers.each((i, container) => {
+      this.sendImage($(container));
+    });
   }
 
-  /**
-   * Валидирует изображение и отправляет его на сервер
-   */
   sendImage(imageContainer) {
-
+    const inputDiv = imageContainer.find('.input');
+    const input = inputDiv.find('input');
+    const path = input.val().trim();
+    if (!path) {
+      inputDiv.addClass('error');
+      return;
+    }
+    inputDiv.addClass('disabled');
+    const imgSrc = imageContainer.find('img').attr('src');
+    Yandex.uploadFile(path, imgSrc, (err, response) => {
+      if (err) {
+        alert('Ошибка загрузки: ' + err);
+        inputDiv.removeClass('disabled');
+      } else {
+        imageContainer.remove();
+        if (this.content.find('.image-preview-container').length === 0) {
+          this.close();
+        }
+      }
+    });
   }
 }
