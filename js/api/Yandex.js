9ba91793b1321b8dde02fd12ea8f2de83af08536
Alexandr-VA <alexandrlobash@gmail.com>
2026-04-05 21:40:43 +0300

    Update Yandex.js

--- js/api/Yandex.js
+++ js/api/Yandex.js
@@ -6,38 +6,68 @@
 class Yandex {
   static HOST = 'https://cloud-api.yandex.net/v1/disk';
 
-  /**
-   * Метод формирования и сохранения токена для Yandex API
-   */
-  static getToken(){
-
+  static getToken() {
+    let token = localStorage.getItem('yandex_token');
+    if (!token) {
+      token = prompt('Введите токен Яндекс.Диска');
+      if (token) localStorage.setItem('yandex_token', token);
+    }
+    return token;
   }
 
-  /**
-   * Метод загрузки файла в облако
-   */
-  static uploadFile(path, url, callback){
-
+  static uploadFile(path, url, callback) {
+    const token = this.getToken();
+    if (!token) {
+      callback('Токен не получен');
+      return;
+    }
+    createRequest({
+      method: 'POST',
+      url: `${this.HOST}/resources/upload`,
+      data: { path: path, url: url },
+      headers: { 'Authorization': `OAuth ${token}` },
+      callback: (err, response) => {
+        if (err) callback(err);
+        else callback(null, response);
+      }
+    });
   }
 
-  /**
-   * Метод удаления файла из облака
-   */
-  static removeFile(path, callback){
-
+  static removeFile(path, callback) {
+    const token = this.getToken();
+    if (!token) {
+      callback('Токен не получен');
+      return;
+    }
+    createRequest({
+      method: 'DELETE',
+      url: `${this.HOST}/resources`,
+      data: { path: path },
+      headers: { 'Authorization': `OAuth ${token}` },
+      callback: (err, response) => callback(err, response)
+    });
   }
 
-  /**
-   * Метод получения всех загруженных файлов в облаке
-   */
-  static getUploadedFiles(callback){
-
+  static getUploadedFiles(callback) {
+    const token = this.getToken();
+    if (!token) {
+      callback('Токен не получен');
+      return;
+    }
+    createRequest({
+      method: 'GET',
+      url: `${this.HOST}/resources/files`,
+      headers: { 'Authorization': `OAuth ${token}` },
+      callback: (err, response) => callback(err, response)
+    });
   }
 
-  /**
-   * Метод скачивания файлов
-   */
-  static downloadFileByUrl(url){
-
+  static downloadFileByUrl(url) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = '';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   }
 }
