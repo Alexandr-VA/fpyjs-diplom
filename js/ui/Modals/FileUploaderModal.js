c996f4a3d3568a611c9fbbf48add4d6437178150
Alexandr-VA <alexandrlobash@gmail.com>
2026-04-12 18:48:44 +0300

    Update_1 FileUploaderModal.js

--- js/ui/Modals/FileUploaderModal.js
+++ js/ui/Modals/FileUploaderModal.js
@@ -6,12 +6,13 @@ class FileUploaderModal extends BaseModal {
   constructor(element) {
     super(element);
     this.content = this.element.find('.content');
+    this.sendAllButton = this.element.find('.send-all.button');
     this.registerEvents();
   }
 
   registerEvents() {
     this.element.find('.header .x.icon, .close.button').on('click', () => this.close());
-    this.element.find('.send-all.button').on('click', () => this.sendAllImages());
+    this.sendAllButton.on('click', () => this.sendAllImages());
     
     this.content.on('click', (e) => {
       const target = $(e.target);
@@ -21,7 +22,7 @@ class FileUploaderModal extends BaseModal {
       if (target.is('button') || target.closest('button')) {
         const container = target.closest('.image-preview-container');
         if (container) {
-          this.sendImage(container);
+          this.sendImage($(container));
         }
       }
     });
@@ -45,33 +46,52 @@ class FileUploaderModal extends BaseModal {
     `;
   }
 
-  sendAllImages() {
-    const containers = this.content.find('.image-preview-container');
-    containers.each((i, container) => {
-      this.sendImage($(container));
-    });
+  async sendAllImages() {
+    this.sendAllButton.addClass('disabled');
+    const containers = [...this.content.find('.image-preview-container')];
+    
+    for (const container of containers) {
+      await new Promise((resolve) => {
+        this.sendImage($(container), resolve);
+      });
+    }
+    
+    this.sendAllButton.removeClass('disabled');
   }
 
-  sendImage(imageContainer) {
+  sendImage(imageContainer, callback = null) {
     const inputDiv = imageContainer.find('.input');
     const input = inputDiv.find('input');
+    const button = inputDiv.find('button');
     const path = input.val().trim();
+    
     if (!path) {
       inputDiv.addClass('error');
+      alert('Укажите путь к файлу на Яндекс.Диске');
+      if (callback) callback();
       return;
     }
-    inputDiv.addClass('disabled');
+    
+    input.prop('disabled', true);
+    button.addClass('disabled');
+    inputDiv.removeClass('error');
+    
     const imgSrc = imageContainer.find('img').attr('src');
     Yandex.uploadFile(path, imgSrc, (err, response) => {
+      input.prop('disabled', false);
+      button.removeClass('disabled');
+      
       if (err) {
-        alert('Ошибка загрузки: ' + err);
-        inputDiv.removeClass('disabled');
+        alert(err.message); // информативное сообщение (например, "уже существует")
+        inputDiv.addClass('error');
       } else {
         imageContainer.remove();
         if (this.content.find('.image-preview-container').length === 0) {
           this.close();
         }
       }
+      
+      if (callback) callback();
     });
   }
 }
